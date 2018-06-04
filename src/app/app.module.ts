cc57d26cadfe67b63947c06b6f131078e3ff18d1
alanj853 <alanjackson.nuig@gmail.com>
2018-06-04 12:01:07 +0100

    created all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,27 @@ import { NgModule } from '@angular/core';
 import { FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
+import { ItemComponent } from './recipes/recipe-list/recipe/item/item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 
 
 
 @NgModule({
   // we register new components in the declarations property
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    ItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+// @ is a decortator
+@Component(
+    {
+        selector: 'app-header',
+        templateUrl: './header.component.html'
+    }
+)
+
+export class HeaderComponent {
+
+}
